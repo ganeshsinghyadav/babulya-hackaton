ea8ef2b724fa04313513d0f43f949be592fc69c6
Denys Butenko <ua13dark@gmail.com>
2014-12-13 22:22:26 +0200

    Add authenticate modal

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -1,4 +1,11 @@
 'use strict';
 
 angular.module('babulya')
-  .controller('NavbarCtrl', function () {});
+  .controller('NavbarCtrl', function ($scope, $modal, $log) {
+    $scope.open = function(size) {
+      var modalInstance = $modal.open({
+        templateUrl: 'app/auth/authModal.html',
+        size: size,
+      });
+    };
+  });
